be3a48f77d942fa80799e19e37f56b396baec58c
KevinP <kevin@londonfoster.com>
2024-05-31 23:29:02 +0800

    Update on listings

--- src/app/listing/page.tsx
+++ src/app/listing/page.tsx
@@ -35,8 +35,8 @@ export default async function Page({
                 <Image className="absolute inset-0 w-full h-full object-cover" width={2000} height={2000} priority alt="Banner" src="https://images.pexels.com/photos/3935333/pexels-photo-3935333.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
                 <div className="relative z-10 max-w-3xl m-auto">
                     <div className="mb-12">
-                        <h1 className="text-5xl text-white font-bold mb-6">Premier Real Estate Solutions</h1>
-                        <p className="text-white">Explore luxury homes, family-friendly neighborhoods, and investment properties. Our expert team offers personalized service and deep market knowledge to help you find the perfect property. Start your journey with us today!</p>
+                        <h1 className="text-4xl text-white font-bold mb-6">Empowering Your Real Estate Experience</h1>
+                        <p className="text-white">"Finding Homes, Building Dreams – Where Families Thrive Together"</p>
                     </div>
                     <div className="flex max-w-lg w-full bg-white rounded-full py-4 pr-4 pl-8 mx-auto items-center space-x-2">
                         <Input type="text" className="bg-white h-auto" placeholder="Enter your home address..." style={{ padding: 'calc(1rem - 1px) 1.25rem' }} />
